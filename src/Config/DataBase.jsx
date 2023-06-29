2f3ab1e473b42f177a45b37d5474bcf8e3c4869e
desarrollo <desarrollo@academicos.cesde.edu.co>
2023-06-29 11:54:37 -0500

    configuraciones

--- src/Config/DataBase.jsx
+++ src/Config/DataBase.jsx
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from '@firebase/firestore'
 const firebaseConfig = {
-    apiKey: "AIzaSyA9Vb-O-tbSHYZ8tSSCcS1X6JDhxip2Ne4",
-    authDomain: "crud-e0e99.firebaseapp.com",
-    projectId: "crud-e0e99",
-    storageBucket: "crud-e0e99.appspot.com",
-    messagingSenderId: "1086110366757",
-    appId: "1:1086110366757:web:71fc8b315acc90ed6e13cc"
-  };
+  apiKey: "AIzaSyDqk76kkYnmoo6D6ZOKTLVjP2ax5b_lgAA",
+  authDomain: "finalweb2-21e65.firebaseapp.com",
+  projectId: "finalweb2-21e65",
+  storageBucket: "finalweb2-21e65.appspot.com",
+  messagingSenderId: "1021686696672",
+  appId: "1:1021686696672:web:90ce76ede15e10b5d7de85"
+};
 const app = initializeApp(firebaseConfig);
 export const DataBase = getFirestore(app)
